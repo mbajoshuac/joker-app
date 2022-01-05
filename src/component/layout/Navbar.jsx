d20ec333cc55817ea246b6005f9f951587a4fd3c
Joshua Mba <angelicjoekeyz@gmail.com>
2022-01-05 23:28:35 +0100

    Feat: App setup

---
+++ src/component/layout/Navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+	render() {
+		return (
+			<div>
+				<nav class="navbar navbar-light bg-light">
+					<a class="navbar-brand" href="#">
+						Joker Application
+					</a>
+					
+				</nav>
+			</div>
+		);
+	}
+}
+
+
+export default Navbar;
