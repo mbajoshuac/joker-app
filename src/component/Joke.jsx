220980c31aee695c729217f098929380c83862ac
Joshua Mba <angelicjoekeyz@gmail.com>
2022-01-08 21:09:28 +0100

    Feat:Complete the joke api request and rendering

--- src/component/Joke.jsx
+++ src/component/Joke.jsx
@@ -3,10 +3,17 @@ import React, { useState, Component, useEffect } from 'react';
 
 
 const Joke = () => {
-    const [joker, setJoker] = useState(initialJoke);
-    useEffect(() => {
-        axios.get('https://icanhazdadjoke.com/api',{headers:{Accept:'application/json'}})
-    })
+    const [joker, setJoker] = useState("joke");
+
+    useEffect(() => {axiosRequest()}, [])
+    const fetchJoke = () => { axiosRequest() }
+    
+      const axiosRequest = () => {
+         axios
+            .get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } })
+            .then(res => setJoker(res.data))
+            .catch(err => console.log(err))
+    }
     return (
         
         <div className="col-md-6">
@@ -16,8 +23,8 @@ const Joke = () => {
                 </div>
                 <div className="card-body">
                     {/* <h5 className="card-title">Special title treatment</h5> */}
-                    <p className="card-text">{joker}</p>
-                    <a href="#" className="btn btn-primary">Get a new Jokee</a>
+                    <p className="card-text">{joker.joke}</p>
+                    <a href="#" className="btn btn-primary" onClick={fetchJoke} >Get a new Jokee</a>
                 </div>
                 </div>
         </div>
